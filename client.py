3c28a93a7dd86c9af1ae7f034505d145f2a4f9bd
saites <saites2001@gmail.com>
2014-01-28 09:24:30 -0500

    "Last time I said it works? I was kidding.  Try this."

---
+++ client.py
@@ -0,0 +1,105 @@
+import xmlrpclib
+import httplib
+import sys
+import socket
+
+def deposit(acnt, amt):
+    global server
+    if amt <= 0:
+        print("Amount should be positive")
+    elif server.deposit(acnt, amt):
+        print("Successfully deposit ${} to account {}!".format(amt, acnt))
+    else:
+        print("No such user or account, {}!".format(acnt))
+
+def withdraw(acnt, amt):
+    global server
+    if amt <= 0:
+        print("Amount should be positive")
+    elif server.withdraw(acnt, amt):
+        print("Successfully withdraw ${} from account {}!".format(amt,acnt))
+    else:
+        print("No such user or account, {}!".format(acnt))
+    
+def inquire(acnt):
+    global server
+    amt = server.inquire(acnt)
+    if amt is not None:
+        print("The current balance of user {} is ${}".format(acnt, amt))
+    else:
+        print("No such user or account, {}!".format(acnt))
+        
+def transfer(acnt1, acnt2, amt):
+    global server
+    if not server.withdraw(acnt1, amt):
+        print("No such user or account, {}!".format(acnt1))
+        return
+    if not server.deposit(acnt2, amt):
+        server.deposit(acnt1, amt)
+        print("No such user or account, {}!".format(acnt2))
+        return
+    print("Successfully transferred ${} from {} to {}!"
+            .format(amt, acnt1, acnt2))
+        
+def parsecmd(cmd):
+    if len(cmd) == 2 and cmd[0] == 'inquire':
+        inquire(int(cmd[1]))
+    elif len(cmd) == 3 and cmd[0] == 'deposit':
+        deposit(int(cmd[1]), int(cmd[2]))
+    elif len(cmd) == 3 and cmd[0] == 'withdraw':
+        withdraw(int(cmd[1]), int(cmd[2]))
+    elif len(cmd) == 4 and cmd[0] == 'transfer':
+        transfer(int(cmd[1]), int(cmd[2]), int(cmd[3]))
+    else:
+        return False
+    return True
+        
+def parsefile(file):
+    try:
+        f = open(file, 'r')
+        for line in f:
+            cmd = line.rstrip().split(' ')
+            if not parsecmd(cmd):
+                print("unknown command '{}'".format(" ".join(cmd)))
+    except socket.timeout:
+        raise
+    except IOError as e:
+        print("unable to open '{}'".format(file))
+
+class TimeoutTransport(xmlrpclib.Transport):
+    timeout = 10.0
+    def set_timeout(self, timeout):
+        self.timeout = timeout
+    def make_connection(self, host):
+        h = httplib.HTTPConnection(host, timeout=self.timeout)
+        return h      
+
+def main():
+    global server
+    try:
+        (address, port_num) = sys.argv[1].split(':')
+        server = xmlrpclib.ServerProxy('http://'+address+':'+port_num,
+            transport=TimeoutTransport())
+        if len(sys.argv) == 3:
+            parsefile(sys.argv[2])
+        elif not parsecmd(sys.argv[2:]):
+            raise IndexError()
+    except (IndexError, ValueError) as e:
+        print("use: python client.py server:port [cmd acnt(s) amt(s) | file]")
+        print("     where cmd is [deposit | withdraw | inquire | transfer]")
+        print("")
+        print("     examples:")
+        print("       deposit 1000 100")
+        print("       withdraw 1000 100")
+        print("       inquire 1000")
+        print("       transfer 1000 1001 100 [from 1000 to 1001]")
+    except socket.error as e:
+        print e
+        print("Unable to establish connection. Try checking the port number")
+    except (xmlrpclib.Fault, xmlrpclib.ProtocolError):
+        print("Unable to establish connection. Try checking the server address")
+        print("or network connection")
+    except Exception as e:
+        print("Unknown error: ", e)
+        
+main()
---
+++ server.py
@@ -0,0 +1,74 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from sys import argv
+from socket import gethostname
+ACCOUNT_FILE = "accounts.txt"
+
+def deposit(acnt, amt): 
+    return change_val(acnt, amt)
+
+def withdraw(acnt, amt):
+    return change_val(acnt, -1*amt)
+
+def inquire(acnt):
+    accnts = get_accounts()
+    try:
+        amt = accnts[acnt]
+        return amt
+    except KeyError:
+        return None
+        
+def change_val(acnt, amt):
+    accnts = get_accounts()
+    try:
+        accnts[acnt] += amt
+        write_accounts(accnts)
+        return True
+    except(KeyError):
+        return False
+
+def get_accounts():
+    accnts = {}
+    try:
+        f = open(ACCOUNT_FILE, 'r')
+        for line in f:
+            vals = line.rstrip().partition(',')
+            accnts[int(vals[0])] = int(vals[2])
+        f.close()
+    except IOError:
+        print("unable to open {}".format(ACCOUNT_FILE))
+    return accnts
+    
+def write_accounts(accnts):
+    try:
+        f = open(ACCOUNT_FILE, 'w')
+        for (key,val) in accnts.iteritems():
+            f.write("{},{}\n".format(key,val))
+        f.close()
+    except IOError:
+        print("unable to open {}".format(ACCOUNT_FILE))
+        
+def main():
+    try:
+        if len(argv) != 2:
+            raise IndexError()
+        port_num = int(argv[1])
+    except IndexError:
+        print("use: python server.py port_num")
+        exit()
+        
+    try:    
+        server = SimpleXMLRPCServer(
+            (gethostname()+".eecs.utk.edu", port_num),
+            allow_none=True)
+    except:
+        print("unable to create server")
+        exit()
+        
+    server.register_introspection_functions()
+    server.register_function(deposit)
+    server.register_function(withdraw)
+    server.register_function(inquire)
+    server.serve_forever()
+    
+main()
